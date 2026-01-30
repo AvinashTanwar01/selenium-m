5e1b7896e8b989a0e0b54c1f1f95916d3c5deb4e
Avinash Tanwar <avinash.tanwar@mphasis.com>
2026-01-30 11:56:04 +0530

    new f

--- TestingNG_1221-documentation/src/main/java/pagesDocument/BaseClass.java
+++ TestingNG_1221-documentation/src/main/java/pagesDocument/BaseClass.java
@@ -1,15 +1,49 @@
 package pagesDocument;
 
+import java.io.FileReader;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseClass {
-	public WebDriver driver;    
 
-   public BaseClass() {
+    public static WebDriver driver;
+    private final Properties prop = new Properties();
 
-  	driver= new EdgeDriver();
-  }
+    public BaseClass() {
+ 
+        try {
+            FileReader fr = new FileReader(
+                "C:\\Users\\avinash.tanwar\\eclipse-workspace\\SeleniumTrainningProject\\data\\data1.properties"
+            );
+            prop.load(fr);
+
+            String browser = prop.getProperty("Browser", "Chrome");
+
+            switch (browser) {
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "FireFox":
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "Edge":
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+            driver.manage().window().maximize();
+        } catch (Exception e) {
+            // If anything fails, fall back to Chrome
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+    }
 }
